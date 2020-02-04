800f38f697192fce774be52b88e102706d78d1e8
admin <admin@admin-PC>
2020-02-04 15:32:58 +0530

    Adding comment for new branch

--- spring-cloud-employee-producer-client/src/main/java/com/example/SpringCloudEmployeeProducerClientApplication.java
+++ spring-cloud-employee-producer-client/src/main/java/com/example/SpringCloudEmployeeProducerClientApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+//This commit is for new branch
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SpringCloudEmployeeProducerClientApplication {
