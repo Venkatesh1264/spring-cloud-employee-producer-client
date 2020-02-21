82a7cec2fae867712fd1e82b24a2a569d5de182f
admin <admin@admin-PC>
2020-02-21 18:15:18 +0530

    employee controller comment

--- spring-cloud-employee-producer-client/src/main/java/com/example/controller/EmployeeController.java
+++ spring-cloud-employee-producer-client/src/main/java/com/example/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bean.Employee;
-
+//This is form employee controller
 @RestController
 public class EmployeeController {
 	@RequestMapping(value="employees",method=RequestMethod.GET)
